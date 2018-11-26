473f1b989dcc36fc50ea888c49accb8ad99db930
Moshe Lawlor <moshelawlor@gmail.com>
2018-11-26 23:25:18 +0000

    odd.js exercise with 'isOdd' function complete

--- hello.js
+++ hello.js
@@ -1,7 +1,6 @@
-function sayHello(name) {
-  console.log("Hello, " + name);
+function returnSayHello(name) {
+  return "Hello, " + name;
 }
 
-sayHello("Caliban");
-sayHello("Mirando");
-sayHello("Ferdinand");
+var greeting = returnSayHello('John');
+console.log(greeting);
